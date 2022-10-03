5aeef6c457080e3482a32a5872b372a899bf93ab
H Brown <hebrown12@gmail.com>
2022-10-03 15:19:48 -0500

    working on frontend portion of app; using react

---
+++ hackernews-react-apollo/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LinkList from './LinkList';
+// import './../styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <LinkList />
+    </div>
+  );
+}
+
+export default App;
