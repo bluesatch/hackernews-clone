8cc64be19c484abd037f1ace6c0150399dd64e99
H Brown <hebrown12@gmail.com>
2022-10-05 14:09:30 -0500

    added authentication

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from '@apollo/client/link/context'
+import { AUTH_TOKEN } from './constants';
 import './styles/index.css';
 import App from './components/App';
 
@@ -11,12 +13,24 @@ import {
   InMemoryCache
 } from '@apollo/client'
 
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
